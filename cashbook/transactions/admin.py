23f1e3df821b582e48d496fd10ece0920f64981f
noxan <noxan@redmoonstudios.de>
2012-09-21 05:55:27 +0200

    add transactionproduct as tabular inline to transaction admin

--- cashbook/transactions/admin.py
+++ cashbook/transactions/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
 
-from models import Transaction
+from models import Transaction, TransactionProduct
 
+
+class TransactionProductInline(admin.TabularInline):
+    model = TransactionProduct
+    extra = 1
 
 class TransactionAdmin(admin.ModelAdmin):
+    inlines = (TransactionProductInline, )
+
     def save_model(self, request, obj, form, change):
         try:
             obj.owner
