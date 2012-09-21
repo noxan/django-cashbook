08b7c07b13466630ded9c614320a29200409a40d
noxan <noxan@redmoonstudios.de>
2012-09-21 08:32:28 +0200

    add translated verbose names to product models

--- cashbook/products/models.py
+++ cashbook/products/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(_("Name"), max_length=255)
 
     def __unicode__(self):
         return self.name
+
+    class Meta:
+        verbose_name = _("Product")
+        verbose_name_plural = _("Products")
