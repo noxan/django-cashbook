c8bb57f9bebb39e1f67ea685d48754f980b5588a
noxan <noxan@redmoonstudios.de>
2012-09-21 08:29:13 +0200

    add translated verbose names to merchants app

--- cashbook/merchants/models.py
+++ cashbook/merchants/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Merchant(models.Model):
-    name = models.CharField(max_length=255)
-    website = models.URLField(blank=True)
+    name = models.CharField(_("Name"), max_length=255)
+    website = models.URLField(_("Website"), blank=True)
 
     def __unicode__(self):
         return self.name
+
+    class Meta:
+        verbose_name = _("Merchant")
+        verbose_name_plural = _("Merchants")
