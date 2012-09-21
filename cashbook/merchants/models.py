08b6e81a9093c9a4184cbaa93d8c98e5a0222e5b
noxan <noxan@redmoonstudios.de>
2012-09-21 05:51:13 +0200

    add optional website field to merchant model

--- cashbook/merchants/models.py
+++ cashbook/merchants/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Merchant(models.Model):
     name = models.CharField(max_length=255)
+    website = models.URLField(blank=True)
 
     def __unicode__(self):
         return self.name
