751271f165eecb832e1ac412f8c83cdcd1293ff5
noxan <noxan@redmoonstudios.de>
2012-09-21 06:04:33 +0200

    implement basic country, state, city and address models

--- cashbook/addresses/models.py
+++ cashbook/addresses/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class State(models.Model):
+    name = models.CharField(max_length=255)
+    country = models.ForeignKey(Country)
+
+
+class City(models.Model):
+    name = models.CharField(max_length=255)
+    state = models.ForeignKey(State)
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    street_number = models.PositiveSmallIntegerField()
+    city = models.ForeignKey(City)
