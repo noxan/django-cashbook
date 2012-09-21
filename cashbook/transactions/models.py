bc40d894813fa13799686c3037ccf38094654b16
noxan <noxan@redmoonstudios.de>
2012-09-21 05:54:34 +0200

    set default value to 0 for currency in transaction model

--- cashbook/transactions/models.py
+++ cashbook/transactions/models.py
@@ -12,7 +12,7 @@ class Transaction(models.Model):
     date = models.DateField()
     merchant = models.ForeignKey(Merchant)
     products = models.ManyToManyField(Product, through='TransactionProduct')
-    currency = models.ForeignKey(Currency)
+    currency = models.ForeignKey(Currency, default=0)
 
     def __unicode__(self):
         return u'%s Transaction from %s' % (unicode(self.date), unicode(self.merchant))
