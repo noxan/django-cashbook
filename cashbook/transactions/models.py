e47c91844313406cb9209beef65c793dce8b1f86
noxan <noxan@redmoonstudios.de>
2012-09-21 05:54:01 +0200

    implement advanced through model for transaction m2m field to products

--- cashbook/transactions/models.py
+++ cashbook/transactions/models.py
@@ -11,8 +11,14 @@ class Transaction(models.Model):
     owner = models.ForeignKey(User, editable=False)
     date = models.DateField()
     merchant = models.ForeignKey(Merchant)
-    products = models.ManyToManyField(Product)
+    products = models.ManyToManyField(Product, through='TransactionProduct')
     currency = models.ForeignKey(Currency)
 
     def __unicode__(self):
         return u'%s Transaction from %s' % (unicode(self.date), unicode(self.merchant))
+
+class TransactionProduct(models.Model):
+    transaction = models.ForeignKey(Transaction)
+    product = models.ForeignKey(Product)
+    quantity = models.PositiveSmallIntegerField(default=1)
+    price = models.DecimalField(max_digits=12, decimal_places=2)
