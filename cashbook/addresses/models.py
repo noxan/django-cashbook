6e419773d7e4f20663ac742b33bbf70288ca8f02
noxan <noxan@redmoonstudios.de>
2012-09-21 08:32:17 +0200

    use ugettext instead of gettext in models for translations

--- cashbook/addresses/models.py
+++ cashbook/addresses/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import ugettext_lazy as _
 
 class Country(models.Model):
     name = models.CharField(_("Name"), max_length=255)
