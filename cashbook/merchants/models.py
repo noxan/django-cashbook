384d71afaaa02726abef3420d2db1b854d47735d
noxan <noxan@redmoonstudios.de>
2012-09-23 20:55:23 +0200

    add optional address field to merchant model

--- cashbook/merchants/models.py
+++ cashbook/merchants/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+from cashbook.addresses.models import Address
+
 
 class Merchant(models.Model):
     name = models.CharField(_("Name"), max_length=255)
     website = models.URLField(_("Website"), blank=True)
+    address = models.ForeignKey(Address, verbose_name=_("Address"), blank=True)
 
     def __unicode__(self):
         return self.name
