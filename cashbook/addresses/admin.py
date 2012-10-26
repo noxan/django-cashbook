94d488720e9ac08e2b4b0e4e7fb5e2245708f439
noxan <noxan@byteweaver.net>
2012-10-26 13:48:14 +0200

    add admin for new street model

--- cashbook/addresses/admin.py
+++ cashbook/addresses/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from cashbook.addresses.models import Country, State, City, Address
+from cashbook.addresses.models import Country, State, City, Street, Address
 
 
 admin.site.register(Country)
 admin.site.register(State)
 admin.site.register(City)
+admin.site.register(Street)
 admin.site.register(Address)
