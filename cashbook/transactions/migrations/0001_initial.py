a8fbc3eb76c2b3030070cfdb1e7c090865a9dd02
noxan <noxan@byteweaver.net>
2012-10-26 14:57:05 +0200

    create initial migrations for transactions app

---
+++ cashbook/transactions/migrations/0001_initial.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Transaction'
+        db.create_table('transactions_transaction', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('owner', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+            ('date', self.gf('django.db.models.fields.DateField')()),
+            ('merchant', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['merchants.Merchant'])),
+            ('currency', self.gf('django.db.models.fields.related.ForeignKey')(default=0, to=orm['currencies.Currency'])),
+            ('value', self.gf('django.db.models.fields.DecimalField')(default=0.0, max_digits=12, decimal_places=2, blank=True)),
+            ('comment', self.gf('django.db.models.fields.TextField')(blank=True)),
+        ))
+        db.send_create_signal('transactions', ['Transaction'])
+
+        # Adding model 'TransactionProduct'
+        db.create_table('transactions_transactionproduct', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('transaction', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['transactions.Transaction'])),
+            ('product', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['products.Product'])),
+            ('quantity', self.gf('django.db.models.fields.PositiveSmallIntegerField')(default=1)),
+            ('price', self.gf('django.db.models.fields.DecimalField')(max_digits=12, decimal_places=2)),
+        ))
+        db.send_create_signal('transactions', ['TransactionProduct'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Transaction'
+        db.delete_table('transactions_transaction')
+
+        # Deleting model 'TransactionProduct'
+        db.delete_table('transactions_transactionproduct')
+
+
+    models = {
+        'addresses.address': {
+            'Meta': {'object_name': 'Address'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'street': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['addresses.Street']"}),
+            'street_number': ('django.db.models.fields.PositiveSmallIntegerField', [], {})
+        },
+        'addresses.city': {
+            'Meta': {'object_name': 'City'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'state': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['addresses.State']"}),
+            'zipcode': ('django.db.models.fields.PositiveIntegerField', [], {})
+        },
+        'addresses.country': {
+            'Meta': {'object_name': 'Country'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'addresses.state': {
+            'Meta': {'object_name': 'State'},
+            'country': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['addresses.Country']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'addresses.street': {
+            'Meta': {'object_name': 'Street'},
+            'city': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['addresses.City']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'street': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'currencies.currency': {
+            'Meta': {'object_name': 'Currency'},
+            'code': ('django.db.models.fields.CharField', [], {'max_length': '3'}),
+            'factor': ('django.db.models.fields.DecimalField', [], {'max_digits': '10', 'decimal_places': '4'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_default': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '25'}),
+            'symbol': ('django.db.models.fields.CharField', [], {'max_length': '1'})
+        },
+        'merchants.merchant': {
+            'Meta': {'object_name': 'Merchant'},
+            'address': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['addresses.Address']", 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'phone': ('django.db.models.fields.CharField', [], {'max_length': '128', 'null': 'True', 'blank': 'True'}),
+            'website': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'})
+        },
+        'products.product': {
+            'Meta': {'object_name': 'Product'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'transactions.transaction': {
+            'Meta': {'object_name': 'Transaction'},
+            'comment': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'currency': ('django.db.models.fields.related.ForeignKey', [], {'default': '0', 'to': "orm['currencies.Currency']"}),
+            'date': ('django.db.models.fields.DateField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'merchant': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['merchants.Merchant']"}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'products': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['products.Product']", 'through': "orm['transactions.TransactionProduct']", 'symmetrical': 'False'}),
+            'value': ('django.db.models.fields.DecimalField', [], {'default': '0.0', 'max_digits': '12', 'decimal_places': '2', 'blank': 'True'})
+        },
+        'transactions.transactionproduct': {
+            'Meta': {'object_name': 'TransactionProduct'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'price': ('django.db.models.fields.DecimalField', [], {'max_digits': '12', 'decimal_places': '2'}),
+            'product': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['products.Product']"}),
+            'quantity': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '1'}),
+            'transaction': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['transactions.Transaction']"})
+        }
+    }
+
+    complete_apps = ['transactions']
