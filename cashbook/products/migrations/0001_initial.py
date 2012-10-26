dd3427b865f6c9b5ba2f24dd7ae62db8714453ef
noxan <noxan@byteweaver.net>
2012-10-26 14:56:56 +0200

    create initial migrations for products app

---
+++ cashbook/products/migrations/0001_initial.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Product'
+        db.create_table('products_product', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
+        ))
+        db.send_create_signal('products', ['Product'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Product'
+        db.delete_table('products_product')
+
+
+    models = {
+        'products.product': {
+            'Meta': {'object_name': 'Product'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        }
+    }
+
+    complete_apps = ['products']
