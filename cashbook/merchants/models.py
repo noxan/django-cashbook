87956c7be3d451569abd8b1f582dfa95b03f174a
noxan <noxan@byteweaver.net>
2012-10-26 14:00:15 +0200

    add optional phone number field to merchant

--- cashbook/merchants/models.py
+++ cashbook/merchants/models.py
@@ -7,6 +7,7 @@ from cashbook.addresses.models import Address
 class Merchant(models.Model):
     name = models.CharField(_("Name"), max_length=255)
     website = models.URLField(_("Website"), blank=True)
+    phone = models.CharField(_("Phone number"), blank=True, null=True, max_length=128)
     address = models.ForeignKey(Address, verbose_name=_("Address"), blank=True)
 
     def __unicode__(self):
