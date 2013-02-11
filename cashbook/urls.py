2e221473a5cbe3d0dab8688a264d20dd14b3aa95
noxan <noxan@byteweaver.net>
2013-02-11 21:40:05 +0100

    add template view to test base template to views

--- cashbook/urls.py
+++ cashbook/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(template_name='base.html'), name='home'),
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
