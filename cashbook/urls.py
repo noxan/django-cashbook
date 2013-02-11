54285d47aaff2a498ee00e98be52d643f4fd15d9
noxan <noxan@byteweaver.net>
2013-02-11 21:39:55 +0100

    remove dummy comments from views

--- cashbook/urls.py
+++ cashbook/urls.py
@@ -5,9 +5,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'cashbook.views.home', name='home'),
-    # url(r'^cashbook/', include('cashbook.foo.urls')),
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
