a290f09709f3e61d2f65f2c793ff0d689c221782
noxan <noxan@byteweaver.net>
2013-02-11 22:07:39 +0100

    include merchants urls in cashbook urls

--- cashbook/urls.py
+++ cashbook/urls.py
@@ -8,6 +8,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='base.html'), name='home'),
 
+    url(r'^merchants/', include('cashbook.merchants.urls', namespace='merchants')),
+
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
