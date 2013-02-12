39fa3c509f22a67f0284b5ae5efedc99d9713a5b
noxan <noxan@byteweaver.net>
2013-02-12 17:41:42 +0100

    include transactions urls in global urls config

--- cashbook/urls.py
+++ cashbook/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='base.html'), name='home'),
 
     url(r'^merchants/', include('cashbook.merchants.urls', namespace='merchants')),
+    url(r'^transactions/', include('cashbook.transactions.urls', namespace='transactions')),
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
