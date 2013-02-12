6a1bd007804e2e1a7486020ae1a1806efe32af34
noxan <noxan@byteweaver.net>
2013-02-12 22:53:11 +0100

    implement merchant create view

--- cashbook/merchants/views.py
+++ cashbook/merchants/views.py
@@ -1,7 +1,12 @@
-from django.views.generic import ListView
+from django.views.generic import CreateView, ListView
 
 from models import Merchant
 
 
 class MerchantListView(ListView):
     model = Merchant
+
+
+class MerchantCreateView(CreateView):
+    model = Merchant
+    template_name = 'merchants/merchant_create.html'
